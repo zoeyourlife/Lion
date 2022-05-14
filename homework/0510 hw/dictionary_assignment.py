c750e86d844a69da28e8d4c60db0c58b60dc9dc9
zoeyourlife <201814112@office.skhu.ac.kr>
2022-05-14 19:57:51 +0900

    0510 hw

---
+++ homework/0510 hw/dictionary_assignment.py
@@ -0,0 +1,37 @@
+최재훈 = {"이름": "최재훈", "나이": 24, "생일": 19990909, "성별": "남", "전화번호": "01047370960"}
+돌돌이 = {"이름": "돌돌이", "나이": 23, "생일": 20000123, "성별": "남", "전화번호": "01048482828"}
+돌순이 = {"이름": "돌순이", "나이": 22, "생일": 20010412, "성별": "여", "전화번호": "01099332233"}
+
+nameList=[]
+ageList=[]
+birthList=[]
+sexList=[]
+pnList=[]
+
+nameList.append(최재훈["이름"])
+nameList.append(돌돌이["이름"])
+nameList.append(돌순이["이름"])
+
+ageList.append(최재훈["나이"])
+ageList.append(돌돌이["나이"])
+ageList.append(돌순이["나이"])
+
+birthList.append(최재훈["생일"])
+birthList.append(돌돌이["생일"])
+birthList.append(돌순이["생일"])
+
+sexList.append(최재훈["성별"])
+sexList.append(돌돌이["성별"])
+sexList.append(돌순이["성별"])
+
+pnList.append(최재훈["전화번호"])
+pnList.append(돌돌이["전화번호"])
+pnList.append(돌순이["전화번호"])
+
+print(nameList)
+print(ageList)
+print(birthList)
+print(sexList)
+print(pnList)
+
+
---
+++ homework/0510 hw/list_assignment.py
@@ -0,0 +1,19 @@
+alphabet =["a","b","c","d","e","f","g","h","i","j","k","l","m","n"
+           ,"o","p","q","r","s","t","u","v","w","x","y","z"]
+number =["0","1","2","3","4","5","6","7","8","9"]
+
+sen1=alphabet[8]+" "+alphabet[0]+alphabet[12]+" "+alphabet[9]+alphabet[0]+alphabet[4]+alphabet[7]+alphabet[20]+alphabet[13]
+sen2=alphabet[8]+" "+alphabet[0]+alphabet[12]+" "+alphabet[19]+alphabet[22]+alphabet[4]+alphabet[4]+alphabet[13]+alphabet[19]+alphabet[24]+" "+alphabet[24]+alphabet[4]+alphabet[0]+alphabet[17]+alphabet[18]+" "+alphabet[14]+alphabet[11]+alphabet[3]
+sen3=alphabet[8]+" "+alphabet[11]+alphabet[8]+alphabet[10]+alphabet[4]+" "+alphabet[1]+alphabet[11]+alphabet[0]+alphabet[2]+alphabet[10]+alphabet[15]+alphabet[8]+alphabet[13]+alphabet[10]
+
+print(sen1)
+print(sen2)
+print(sen3)
+
+num1=number[1]+number[9]+number[9]+number[9]+number[0]+number[9]+number[0]+number[9]
+num2=number[0]+number[1]+number[0]+number[4]+number[7]+number[3]+number[7]+number[0]+number[9]+number[6]+number[0]
+num3=number[2]+number[0]+number[1]+number[8]+number[1]+number[4]+number[1]+number[1]+number[2]
+
+print("제 생일은 " +num1+" 입니다.")
+print("제 번호는 " +num2+" 입니다.")
+print("제 학번은 " +num3+" 입니다.")
