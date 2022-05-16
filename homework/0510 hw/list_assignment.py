8719e92c03334e8d70868ff8224e3c469ddf59e5
zoeyourlife <201814112@office.skhu.ac.kr>
2022-05-16 20:23:55 +0900

    마지막수정

--- homework/0510 hw/list_assignment.py
+++ homework/0510 hw/list_assignment.py
@@ -2,9 +2,9 @@ alphabet =["a","b","c","d","e","f","g","h","i","j","k","l","m","n"
            ,"o","p","q","r","s","t","u","v","w","x","y","z",' ']
 number =["0","1","2","3","4","5","6","7","8","9",'-']
 
-sen1= alphabet[22]+alphabet[4]+" "+alphabet[0]+alphabet[17]+alphabet[4]+" "+alphabet[18]+alphabet[10]+alphabet[7]+alphabet[20]+" "+alphabet[11]+alphabet[8]+alphabet[10]+alphabet[4]+alphabet[11]+alphabet[8]+alphabet[14]+alphabet[13]
-sen2= alphabet[7]+alphabet[0]+alphabet[2]+alphabet[10]+" "+alphabet[24]+alphabet[14]+alphabet[20]+alphabet[17]+" "+alphabet[11]+alphabet[8]+alphabet[5]+alphabet[4]
-sen3= alphabet[12]+alphabet[24]+" "+alphabet[13]+alphabet[0]+alphabet[12]+alphabet[4]+" "+alphabet[8]+alphabet[18]+" "+alphabet[9]+alphabet[0]+alphabet[4]+alphabet[7]+alphabet[20]+alphabet[13]
+sen1= alphabet[22]+alphabet[4]+alphabet[26]+alphabet[0]+alphabet[17]+alphabet[4]+alphabet[26]+alphabet[18]+alphabet[10]+alphabet[7]+alphabet[20]+alphabet[26]+alphabet[11]+alphabet[8]+alphabet[10]+alphabet[4]+alphabet[11]+alphabet[8]+alphabet[14]+alphabet[13]
+sen2= alphabet[7]+alphabet[0]+alphabet[2]+alphabet[10]+alphabet[26]+alphabet[24]+alphabet[14]+alphabet[20]+alphabet[17]+alphabet[26]+alphabet[11]+alphabet[8]+alphabet[5]+alphabet[4]
+sen3= alphabet[12]+alphabet[24]+alphabet[26]+alphabet[13]+alphabet[0]+alphabet[12]+alphabet[4]+alphabet[26]+alphabet[8]+alphabet[18]+alphabet[26]+alphabet[9]+alphabet[0]+alphabet[4]+alphabet[7]+alphabet[20]+alphabet[13]
 
 print(sen1)
 print(sen2)
